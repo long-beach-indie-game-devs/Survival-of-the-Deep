73511941265c0cd66036185f7e86679294ccff4e
Nolan Ohlhieser <legendofnolan@gmail.com>
2019-08-20 22:40:32 -0700

    Added 1.2 got a use function
    got terminal to change color when your staring rigth at it then click. Tomrrow I'm going to have a ray or collistion function so your not using the terminal across the map.

---
+++ Survival of the Deep/Survival-of-the-Deep-46549daf27e3270b7892fbae5f78272e16a68bd7/Survival of the Deep/Assets/Scripts/item.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public enum State
+    //numeration
+    {
+        randomColor,
+        close,
+        inbetween
+    }
+
+    public State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = item.State.close;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("Exit");
+    }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("Up");
+        if (state == item.State.close)
+            Red();
+        else
+            Close();
+        //checking for function
+    }
+
+    private void Red()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        // change animation to changing color
+        state = item.State.randomColor;
+        
+    } 
+
+    private void Close()
+    {
+        gameObject.GetComponent<Renderer> ().material.color = Color.gray;
+        state = item.State.close;
+    }
+}
